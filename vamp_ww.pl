946395c3893dc07eb2d9c5cce69cc6ef0e58d9b8
Nathan Rieb <nrieb@cs.unm.edu>
2014-10-30 22:20:44 -0600

    Abstracted max value (number of vamps / ww).

--- vamp_ww.pl
+++ vamp_ww.pl
@@ -9,45 +9,53 @@ move_n_to(e, Val, New_val, N) :-
 move_n_to(w, Val, New_val, N) :-
     New_val is Val + N.    
 
+max_size(3).
+
 move(state(X, Vamps_west, WWs_west),
      state(Y, New_vamps, WWs_west)) :-
     opp(X, Y),
     move_n_to(Y, Vamps_west, New_vamps, 2),
-    between(0, 3, New_vamps).
+    max_size(Max),
+    between(0, Max, New_vamps).
 
 move(state(X, Vamps_west, WWs_west),
      state(Y, Vamps_west, New_wws)) :-
     opp(X, Y),
     move_n_to(Y, WWs_west, New_wws, 2),
-    between(0, 3, New_wws).
+    max_size(Max),
+    between(0, Max, New_wws).
 
 move(state(X, Vamps_west, WWs_west),
      state(Y, New_vamps, New_wws)) :-
     opp(X, Y),
     move_n_to(Y, WWs_west, New_wws, 1),
-    between(0, 3, New_wws),
+    max_size(Max),
+    between(0, Max, New_wws),
     move_n_to(Y, Vamps_west, New_vamps, 1),
-    between(0, 3, New_vamps).
+    between(0, Max, New_vamps).
 
 move(state(X, Vamps_west, WWs_west),
      state(Y, New_vamps, WWs_west)) :-
     opp(X, Y),
     move_n_to(Y, Vamps_west, New_vamps, 1),
-    between(0, 3, New_vamps).
+    max_size(Max),
+    between(0, Max, New_vamps).
 
 move(state(X, Vamps_west, WWs_west),
      state(Y, Vamps_west, New_wws)) :-
     opp(X, Y),
     move_n_to(Y, WWs_west, New_wws, 1),
-    between(0, 3, New_wws).
+    max_size(Max),
+    between(0, Max, New_wws).
 
 unsafe(state(_, Vamps_west, WWs_west)) :-
     Vamps_west > 0,
     WWs_west > Vamps_west.
 
 unsafe(state(_, Vamps_west, WWs_west)) :-
-    Vamps_east is 3 - Vamps_west,
-    WWs_east is 3 - WWs_west,
+    max_size(Max),
+    Vamps_east is Max - Vamps_west,
+    WWs_east is Max - WWs_west,
     Vamps_east > 0,
     WWs_east > Vamps_east.
 
